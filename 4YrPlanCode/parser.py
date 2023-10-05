13c63f11ebaeab4dc682f1403b2e5de6eca9352e
natepetersen03 <chepic123@gmail.com>
2023-10-05 02:52:34 -0400

    Created scraper using RegEx in Python, it currently stores Course Name, CRN, Subject, and Course Number for undergraduate courses in a dictionary. Next step is putting it in a database and elliminating duplicates since currently there are many of the same courses with different CRNs

---
+++ 4YrPlanCode/parser.py
@@ -0,0 +1,53 @@
+# Parse the HTML code
+import re
+with open('EveryCourse.html', 'r') as f:
+    print("hey")
+    crnFound = False
+    course_sections = f.readlines()
+    course_name = ""
+    crn = 0
+    course_subject = ""
+    course_number = 0
+    # Initialize a dictionary to store course information
+    course_info = {}
+    # Iterate through each course section
+    for section in course_sections:
+    # Extract the course name, course subject, and course number using regex
+        #Matches strings in the ">ANYSTRING - 5*(0-9) - 2-4UPPER (1-4)(3*0-9)" format that is used in the html file
+        match = re.search(r'>[A-Z]{1}.+ - \d{5} - [A-Z]{2,4} [1-4]\d{3}', section)
+        if match:
+            match = match.group(0)
+            match = match.replace(" - ", "* ")
+            match = match.replace(">", "")
+            match = match.replace("  ", " ")
+            l = match.split("* ")
+            if len(l) > 3:
+                l[0] = l[0] + " " + l[1]
+                temp = l[2]
+                l[2] = l[3]
+                l[1] = temp
+            if l[1] in course_sections:
+                continue
+            cSubjAndNum = l[2].split(" ")
+            l[2] = cSubjAndNum[0]
+            l.append(cSubjAndNum[1])
+            #Split the string into a list by removing unnecessary characters
+            course_name = l[0]
+            crn = l[1]
+            course_subject = l[2]
+            course_number = l[3]
+            course_info[crn] = {
+                'Course Name': course_name.strip(),
+                'Course Subject': course_subject.strip(),
+                'Course Number': course_number.strip(),
+                'CRN': crn.strip()
+            }
+            
+                
+
+# Print the course information
+for crn, info in course_info.items():
+    print(f"CRN: {crn}")
+    print(f"Course Name: {info['Course Name']}")
+    print(f"Course Subject: {info['Course Subject']}")
+    print(f"Course Number: {info['Course Number']}")
